ed7cd6ba97ab9a04f1089867ce10acd0281cbdf3
sanya andreichuk <sanya2371@gmail.com>
2021-03-25 17:55:03 +0200

    Update notification id.

--- sdk/Notifo.SDK.FirebasePlugin/NotifoPushNotificationHandler.android.cs
+++ sdk/Notifo.SDK.FirebasePlugin/NotifoPushNotificationHandler.android.cs
@@ -5,6 +5,7 @@
 //  All rights reserved. Licensed under the MIT license.
 // ==========================================================================
 
+using System;
 using System.Collections.Generic;
 using Android.Support.V4.App;
 using Plugin.FirebasePushNotification;
@@ -14,6 +15,17 @@ namespace Notifo.SDK.FirebasePlugin
     internal class NotifoPushNotificationHandler : DefaultPushNotificationHandler
     {
         private const string SubjectKey = "subject";
+        private static readonly Random Random = new Random();
+
+        public override void OnReceived(IDictionary<string, object> parameters)
+        {
+            if (!parameters.ContainsKey(IdKey))
+            {
+                parameters[IdKey] = Random.Next(1, int.MaxValue);
+            }
+
+            base.OnReceived(parameters);
+        }
 
         public override void OnBuildNotification(NotificationCompat.Builder notificationBuilder, IDictionary<string, object> parameters)
         {
