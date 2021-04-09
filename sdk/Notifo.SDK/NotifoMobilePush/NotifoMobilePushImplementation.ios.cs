7747c973340390e9da63fb328525d0cbb2062908
Sanya <sanya2371@gmail.com>
2021-04-09 10:09:07 +0300

    iOS fix local notification actions.

--- sdk/Notifo.SDK/NotifoMobilePush/NotifoMobilePushImplementation.ios.cs
+++ sdk/Notifo.SDK/NotifoMobilePush/NotifoMobilePushImplementation.ios.cs
@@ -49,7 +49,7 @@ namespace Notifo.SDK.NotifoMobilePush
                 var eventArgs = new NotificationEventArgs(notification);
                 OnReceived(eventArgs);
 
-                _ = ShowLocalNotificationAsync(notification);
+                await ShowLocalNotificationAsync(notification);
             }
 
             await TrackNotificationsAsync(notifications);
@@ -66,6 +66,7 @@ namespace Notifo.SDK.NotifoMobilePush
         private async Task ShowLocalNotificationAsync(NotificationDto notification)
         {
             var content = new UNMutableNotificationContent();
+
             content = await EnrichNotificationContentAsync(content, notification);
 
             content.UserInfo = notification.ToDictionary().ToNSDictionary();
