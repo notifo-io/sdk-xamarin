d34f021090fb723c8605fdda604c4fd8b849d328
Oleksandr Andreichuk <sanya2371@gmail.com>
2022-02-15 11:40:46 +0200

    Update Constants.cs

--- sample/Sample/Constants.cs
+++ sample/Sample/Constants.cs
@@ -9,7 +9,7 @@ namespace Sample
 {
     public static class Constants
     {
-        public const string ApiUrl = "https://notifo.easierlife.com";
-        public const string UserApiKey = "7hht0pqtsqyvkeco9h7434wrnbrpld6s1hmdu08e0tkx";
+        public const string ApiUrl = {API_URL};
+        public const string UserApiKey = {USER_API_KEY};
     }
 }
