a1fc43261902389579ba97e552927c3be85b24b3
Nil Busra Bedir <busra.bedir@live.de>
2022-07-28 10:34:49 +0200

    add Plugin.FirebasePushNotification code to project

---
+++ sdk/Notifo.SDK.FirebasePlugin/PluginFirebasePushNotifications/NotificationActionType.shared.cs
@@ -0,0 +1,10 @@
+﻿namespace Plugin.FirebasePushNotification
+{
+    public enum NotificationActionType
+    {
+        Default,
+        AuthenticationRequired, //Only applies for iOS
+        Foreground,
+        Destructive  //Only applies for iOS
+    }
+}
