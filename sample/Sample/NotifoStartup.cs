66d00c49529367f5bc6b23c3ffe66177b95ed6e8
Nil Busra Bedir <bedir@easierlife.de>
2023-01-05 10:21:14 +0100

    fix defualt label text color;  Entry -> Editor for token; UseSaveArea for iOS

--- sample/Sample/NotifoStartup.cs
+++ sample/Sample/NotifoStartup.cs
@@ -17,9 +17,22 @@ namespace Sample
         {
             notifo
                 .SetBaseUrl(Constants.ApiUrl)
+                .SetApiVersion(ApiVersion.Version_1_5)
                 .UseFirebasePluginEventsProvider();
 
             notifo.OnNotificationOpened += Current_OnNotificationOpened;
+            notifo.OnNotificationReceived += Notifo_OnNotificationReceived;
+            notifo.OnLog += Notifo_OnLog;
+        }
+
+        private void Notifo_OnLog(object sender, NotificationLogEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Notification log message: " + e.Message);
+        }
+
+        private void Notifo_OnNotificationReceived(object sender, NotificationEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Notification received: " + e.Notification.Subject);
         }
 
         private void Current_OnNotificationOpened(object source, NotificationEventArgs e)
